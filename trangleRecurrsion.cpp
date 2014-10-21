9bb88d624b16d0d7128ce517819fc12e923cfde8
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-10-21 11:15:53 +0800

    遞迴練習的作業(未完成)

---
+++ trangleRecurrsion.cpp
@@ -0,0 +1,269 @@
+/**************************
+ * Includes
+ *
+ **************************/
+
+#include <windows.h>
+#include <gl/gl.h>
+
+
+/**************************
+ * Function Declarations
+ *
+ **************************/
+
+LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
+WPARAM wParam, LPARAM lParam);
+void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC);
+void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC);
+int n;
+GLfloat v[4][3]={{0.0, 0.0, 1.0}, {0.0, 0.942809, -0.33333},
+      {-0.816497, -0.471405, -0.333333}, {0.816497, -0.471405, -0.333333}};
+
+GLfloat colors[4][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0},
+                        {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}};
+
+/**************************
+ * WinMain
+ *
+ **************************/
+
+
+void triangle(GLfloat *va, GLfloat *vb, GLfloat *vc)
+{
+       glVertex3fv(va);
+       glVertex3fv(vb);
+       glVertex3fv(vc);
+}
+
+void tetra(GLfloat *a, GLfloat *b, GLfloat *c, GLfloat *d)
+{
+    glColor3fv(colors[0]);
+    triangle(a, b, c);
+    glColor3fv(colors[1]);
+    triangle(a, c, d);
+    glColor3fv(colors[2]);
+    triangle(a, d, b);
+    glColor3fv(colors[3]);
+    triangle(b, d, c);
+}
+
+void divide_tetra(GLfloat *a, GLfloat *b, GLfloat *c, GLfloat *d, int m)
+{
+
+    GLfloat mid[6][3];
+    int j;
+    if(m>0)
+    {
+        /* compute six midpoints */
+
+        for(j=0; j<3; j++) mid[0][j]=(a[j]+b[j])/2;
+        for(j=0; j<3; j++) mid[1][j]=(a[j]+c[j])/2;
+        for(j=0; j<3; j++) mid[2][j]=(a[j]+d[j])/2;
+        for(j=0; j<3; j++) mid[3][j]=(b[j]+c[j])/2;
+        for(j=0; j<3; j++) mid[4][j]=(c[j]+d[j])/2;
+        for(j=0; j<3; j++) mid[5][j]=(b[j]+d[j])/2;
+
+        /* create 4 tetrahedrons by subdivision */
+
+        divide_tetra(a, mid[0], mid[1], mid[2], m-1);
+        divide_tetra(mid[0], b, mid[3], mid[5], m-1);
+        divide_tetra(mid[1], mid[3], c, mid[4], m-1);
+        divide_tetra(mid[2], mid[4], d, mid[5], m-1);
+
+    }
+    else(tetra(a,b,c,d)); /* draw tetrahedron at end of recursion */
+}
+
+
+void display()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glBegin(GL_TRIANGLES);
+    divide_tetra(v[0], v[1], v[2], v[3], n);
+    glEnd();
+    glFlush();
+}
+
+
+void myReshape(int w, int h)
+{
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    if (w <= h)
+        glOrtho(-2.0, 2.0, -2.0 * (GLfloat) h / (GLfloat) w,
+            2.0 * (GLfloat) h / (GLfloat) w, -10.0, 10.0);
+    else
+        glOrtho(-2.0 * (GLfloat) w / (GLfloat) h,
+            2.0 * (GLfloat) w / (GLfloat) h, -2.0, 2.0, -10.0, 10.0);
+    glMatrixMode(GL_MODELVIEW);
+    
+}
+
+int WINAPI WinMain (HINSTANCE hInstance,
+                    HINSTANCE hPrevInstance,
+                    LPSTR lpCmdLine,
+                    int iCmdShow)
+{
+    WNDCLASS wc;
+    HWND hWnd;
+    HDC hDC;
+    HGLRC hRC;        
+    MSG msg;
+    BOOL bQuit = FALSE;
+    float theta = 0.0f;
+
+    /* register window class */
+    wc.style = CS_OWNDC;
+    wc.lpfnWndProc = WndProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = hInstance;
+    wc.hIcon = LoadIcon (NULL, IDI_APPLICATION);
+    wc.hCursor = LoadCursor (NULL, IDC_ARROW);
+    wc.hbrBackground = (HBRUSH) GetStockObject (BLACK_BRUSH);
+    wc.lpszMenuName = NULL;
+    wc.lpszClassName = "3D Gasket";
+    RegisterClass (&wc);
+
+    /* create main window */
+    hWnd = CreateWindow (
+      "3D Gasket", "Simple Triangle", // Simple Triangle為視窗上的名字 
+      WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE,
+      0, 0, 256, 256,// 0,0 表示視窗出現的位置 在微軟工作下會出現在左上角  256,256表示視窗大小 
+      NULL, NULL, hInstance, NULL);
+
+    /* enable OpenGL for the window */
+    EnableOpenGL (hWnd, &hDC, &hRC);
+
+    /* program main loop */
+    while (!bQuit)
+    {
+        /* check for messages */
+        if (PeekMessage (&msg, NULL, 0, 0, PM_REMOVE))
+        {
+            /* handle or dispatch messages */
+            if (msg.message == WM_QUIT)
+            {
+                bQuit = TRUE;
+            }
+            else
+            {
+                TranslateMessage (&msg);
+                DispatchMessage (&msg);
+            }
+        }
+        else
+        {
+            /* OpenGL animation code goes here */
+            /**
+            glClearColor (0.0f,0.0f, 0.0f, 1.0f); //背景顏色 
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //清除所有顏色及深度 
+            glEnable(GL_DEPTH_TEST); // 深度測試 
+            
+            glBegin(GL_TRIANGLES); // 開始畫三角形 
+             glColor3f(1.0f,0.0f,0.0f); glVertex2f(0.5f,0.0f); //定義第1個座標點  顏色為紅色 
+             glColor3f(0.0f,1.0f,0.0f); glVertex2f(-0.5f,0.0f); //定義第2個座標點  顏色為綠色  
+             glColor3f(0.0f,0.0f,1.0f); glVertex2f(0.0f,1.0f); //定義第3個座標點  顏色為藍色  
+            glEnd();*/
+            n=2; /* or enter number of subdivision steps here */
+            glEnable(GL_DEPTH_TEST);
+            glClearColor (1.0, 1.0, 1.0, 1.0);
+            display();
+            
+            SwapBuffers (hDC);
+
+            Sleep (1);
+        }
+    }
+
+    /* shutdown OpenGL */
+    DisableOpenGL (hWnd, hDC, hRC);
+
+    /* destroy the window explicitly */
+    DestroyWindow (hWnd);
+
+    return msg.wParam;
+}
+
+
+/********************
+ * Window Procedure
+ *
+ ********************/
+
+LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
+                          WPARAM wParam, LPARAM lParam)
+{
+
+    switch (message)
+    {
+    case WM_CREATE:
+        return 0;
+    case WM_CLOSE:
+        PostQuitMessage (0);
+        return 0;
+
+    case WM_DESTROY:
+        return 0;
+
+    case WM_KEYDOWN:
+        switch (wParam)
+        {
+        case VK_ESCAPE:
+            PostQuitMessage(0);
+            return 0;
+        }
+        return 0;
+
+    default:
+        return DefWindowProc (hWnd, message, wParam, lParam);
+    }
+}
+
+
+/*******************
+ * Enable OpenGL
+ *
+ *******************/
+
+void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC)
+{
+    PIXELFORMATDESCRIPTOR pfd;
+    int iFormat;
+
+    /* get the device context (DC) */
+    *hDC = GetDC (hWnd);
+
+    /* set the pixel format for the DC */
+    ZeroMemory (&pfd, sizeof (pfd));
+    pfd.nSize = sizeof (pfd);
+    pfd.nVersion = 1;
+    pfd.dwFlags = PFD_DRAW_TO_WINDOW | 
+      PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER;
+    pfd.iPixelType = PFD_TYPE_RGBA;
+    pfd.cColorBits = 24;
+    pfd.cDepthBits = 16;
+    pfd.iLayerType = PFD_MAIN_PLANE;
+    iFormat = ChoosePixelFormat (*hDC, &pfd);
+    SetPixelFormat (*hDC, iFormat, &pfd);
+
+    /* create and enable the render context (RC) */
+    *hRC = wglCreateContext( *hDC );
+    wglMakeCurrent( *hDC, *hRC );
+
+}
+
+
+/******************
+ * Disable OpenGL
+ *
+ ******************/
+
+void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC)
+{
+    wglMakeCurrent (NULL, NULL);
+    wglDeleteContext (hRC);
+    ReleaseDC (hWnd, hDC);
+}

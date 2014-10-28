50a768cd782dcf3ece98569eb21aae569f7728df
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-10-28 11:33:36 +0800

    完成旋轉漸層正方體

--- colorTube.cpp
+++ colorTube.cpp
@@ -5,8 +5,7 @@
 
 #include <windows.h>
 #include <gl/gl.h>
-#include <GL/glut.h>
-#include <GL/glut.h>
+#include <gl/glu.h>
 
 
 /**************************
@@ -18,7 +17,75 @@ LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
 WPARAM wParam, LPARAM lParam);
 void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC);
 void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC);
+void polygon(int a, int b, int c , int d);
+void colorcube(void);
+void display(void);
 
+	GLfloat vertices[][3] = {{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},
+	{1.0,1.0,-1.0}, {-1.0,1.0,-1.0}, {-1.0,-1.0,1.0}, 
+	{1.0,-1.0,1.0}, {1.0,1.0,1.0}, {-1.0,1.0,1.0}};
+
+	GLfloat normals[][3] = {{-1.0,-1.0,-1.0},{1.0,-1.0,-1.0},
+	{1.0,1.0,-1.0}, {-1.0,1.0,-1.0}, {-1.0,-1.0,1.0}, 
+	{1.0,-1.0,1.0}, {1.0,1.0,1.0}, {-1.0,1.0,1.0}};
+
+	GLfloat colors[][3] = {{0.0,0.0,0.0},{1.0,0.0,0.0},
+	{1.0,1.0,0.0}, {0.0,1.0,0.0}, {0.0,0.0,1.0}, 
+	{1.0,0.0,1.0}, {1.0,1.0,1.0}, {0.0,1.0,1.0}};
+
+void polygon(int a, int b, int c , int d)
+{
+
+/* draw a polygon via list of vertices */
+
+ 	glBegin(GL_POLYGON);
+		glColor3fv(colors[a]);
+		glNormal3fv(normals[a]);
+		glVertex3fv(vertices[a]);
+		glColor3fv(colors[b]);
+		glNormal3fv(normals[b]);
+		glVertex3fv(vertices[b]);
+		glColor3fv(colors[c]);
+		glNormal3fv(normals[c]);
+		glVertex3fv(vertices[c]);
+		glColor3fv(colors[d]);
+		glNormal3fv(normals[d]);
+		glVertex3fv(vertices[d]);
+	glEnd();
+																										}
+
+void colorcube(void)
+{
+
+/* map vertices to faces */
+
+	polygon(0,3,2,1);
+	polygon(2,3,7,6);
+	polygon(0,4,7,3);
+	polygon(1,2,6,5);
+	polygon(4,5,6,7);
+	polygon(0,1,5,4);
+}
+
+static GLfloat theta[] = {0.0,0.0,0.0};
+static GLint axis = 2;
+
+void display(void)
+{
+/* display callback, clear frame buffer and z buffer,
+   rotate cube and draw, swap buffers */
+
+ glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glLoadIdentity();
+	glRotatef(theta[0], 1.0, 0.0, 0.0);
+	glRotatef(theta[1], 0.0, 1.0, 0.0);
+	glRotatef(theta[2], 0.0, 0.0, 1.0);
+
+ colorcube();
+
+ glFlush();
+	
+}
 
 /**************************
  * WinMain
@@ -55,7 +122,7 @@ int WINAPI WinMain (HINSTANCE hInstance,
     hWnd = CreateWindow (
       "GLSample", "OpenGL Sample", 
       WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE,
-      0, 0, 500, 500,
+      0, 0, 512, 512,
       NULL, NULL, hInstance, NULL);
 
     /* enable OpenGL for the window */
@@ -81,75 +148,25 @@ int WINAPI WinMain (HINSTANCE hInstance,
         else
         {
             /* OpenGL animation code goes here */
-
+			
             glClearColor (0.0f, 0.0f, 0.0f, 0.0f);
-            glClear (GL_COLOR_BUFFER_BIT);
-            
+            glClear (GL_COLOR_BUFFER_BIT| GL_DEPTH_BUFFER_BIT);
+            glEnable(GL_DEPTH_TEST);
+            glMatrixMode(GL_PROJECTION);
+            glLoadIdentity();
+            glOrtho(-5,5,-5,5,-5,5);
             glMatrixMode(GL_MODELVIEW);
-            glLoadIdentity();  
-
-            gluLookAt (0.0, 1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
-            
-            
-            
+			
+			glRotatef (theta, 0.0f, 1.0f, 1.0f);
             glPushMatrix ();
             
-
-            glBegin (GL_POLYGON);
-                    glColor3f (0.0f, 1.0f, 1.0f);  glVertex3f (1.0f, 0.0f,1.0f);
-                                                   glVertex3f (1.0f, 0.0f,0.0f);
-                                                   glVertex3f (1.0f, 1.0f,0.0f);
-                                                   glVertex3f (1.0f, 1.0f,1.0f);        
-            glEnd ();
-            glBegin (GL_POLYGON);
-                    glColor3f (1.0f, 1.0f, 1.0f);  glVertex3f (1.0f, 1.0f,1.0f);
-                                                   glVertex3f (1.0f, 1.0f,0.0f);
-                                                   glVertex3f (0.0f, 1.0f,0.0f);
-                                                   glVertex3f (0.0f, 1.0f,1.0f);
-            glEnd ();
-            glBegin (GL_POLYGON);
-                    glColor3f (1.0f, 1.0f, 0.0f);  glVertex3f (0.0f, 1.0f,1.0f);
-                                                   glVertex3f (0.0f, 1.0f,0.0f);
-                                                   glVertex3f (0.0f, 0.0f,0.0f);
-                                                   glVertex3f (0.0f, 0.0f,1.0f);
-            glEnd ();
-            glBegin (GL_POLYGON);
-                    glColor3f (0.0f, 1.0f, 0.0f);  glVertex3f (0.0f, 0.0f,1.0f);
-                                                   glVertex3f (0.0f, 0.0f,0.0f);
-                                                   glVertex3f (1.0f, 0.0f,0.0f);
-                                                   glVertex3f (1.0f, 0.0f,1.0f);
-            glEnd ();
-            
-            glBegin (GL_POLYGON);
-                                                   glVertex3f (1.0f, 0.0f,1.0f);
-                                                   glVertex3f (1.0f, 1.0f,1.0f);
-                                                   glVertex3f (0.0f, 1.0f,1.0f);
-                                                   glVertex3f (0.0f, 0.0f,1.0f);
-            glEnd ();
-            
-            glBegin (GL_POLYGON);
-                    glColor3f (0.0f, 0.0f, 1.0f);  glVertex3f (1.0f, 0.0f,0.0f);
-                    glColor3f (1.0f, 0.0f, 1.0f);  glVertex3f (1.0f, 1.0f,0.0f);
-                    glColor3f (1.0f, 0.0f, 0.0f);  glVertex3f (0.0f, 1.0f,0.0f);
-                    glColor3f (0.0f, 0.0f, 0.0f);  glVertex3f (0.0f, 0.0f,0.0f);
-            glEnd ();
-               
-                
-                     
-            /**
-            glRotatef (theta, 0.0f, 0.0f, 1.0f);
-            glBegin (GL_TRIANGLES);
-            glColor3f (1.0f, 0.0f, 0.0f);   glVertex2f (0.0f, 1.0f);
-            glColor3f (0.0f, 1.0f, 0.0f);   glVertex2f (0.87f, -0.5f);
-            glColor3f (0.0f, 0.0f, 1.0f);   glVertex2f (-0.87f, -0.5f);
-            glEnd ();*/
+            colorcube();
             glPopMatrix ();
-            
 
             SwapBuffers (hDC);
 
             theta += 1.0f;
-            Sleep (1);
+            Sleep (200);
         }
     }
 

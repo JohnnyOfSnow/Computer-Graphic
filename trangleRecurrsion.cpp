008e4833d8d2c1c03613dd4438317f191fbc31c7
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-10-31 13:28:03 +0800

    著色

--- trangleRecurrsion.cpp
+++ trangleRecurrsion.cpp
@@ -16,72 +16,76 @@ LRESULT CALLBACK WndProc (HWND hWnd, UINT message,
 WPARAM wParam, LPARAM lParam);
 void EnableOpenGL (HWND hWnd, HDC *hDC, HGLRC *hRC);
 void DisableOpenGL (HWND hWnd, HDC hDC, HGLRC hRC);
-int n;
-GLfloat v[4][3]={{0.0, 0.0, 1.0}, {0.0, 0.942809, -0.33333},
-      {-0.816497, -0.471405, -0.333333}, {0.816497, -0.471405, -0.333333}};
+int n = 1;
+typedef float point[3];
 
-GLfloat colors[4][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0},
-                        {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}};
+/* initial tetrahedron */
 
-/**************************
- * WinMain
- *
- **************************/
+point v[]={{0.0, 0.0, 1.0}, {0.0, 0.942809, -0.33333},
+          {-0.816497, -0.471405, -0.333333}, {0.816497, -0.471405, -0.333333}};
+
+static GLfloat theta[] = {0.0,0.0,0.0};
 
 
-void triangle(GLfloat *va, GLfloat *vb, GLfloat *vc)
+
+void triangle( point a, point b, point c)
+
+/* display one triangle using a line loop for wire frame, a single
+normal for constant shading, or three normals for interpolative shading */
 {
-       glVertex3fv(va);
-       glVertex3fv(vb);
-       glVertex3fv(vc);
+    glBegin(GL_POLYGON);
+       glNormal3fv(a);
+       glVertex3fv(a);
+       glVertex3fv(b);
+       glVertex3fv(c);
+    glEnd();
 }
 
-void tetra(GLfloat *a, GLfloat *b, GLfloat *c, GLfloat *d)
-{
-    glColor3fv(colors[0]);
-    triangle(a, b, c);
-    glColor3fv(colors[1]);
-    triangle(a, c, d);
-    glColor3fv(colors[2]);
-    triangle(a, d, b);
-    glColor3fv(colors[3]);
-    triangle(b, d, c);
-}
-
-void divide_tetra(GLfloat *a, GLfloat *b, GLfloat *c, GLfloat *d, int m)
+void divide_triangle(point a, point b, point c, int m)
 {
 
-    GLfloat mid[6][3];
+/* triangle subdivision using vertex numbers
+righthand rule applied to create outward pointing faces */
+
+    point v1, v2, v3;
     int j;
     if(m>0)
     {
-        /* compute six midpoints */
-
-        for(j=0; j<3; j++) mid[0][j]=(a[j]+b[j])/2;
-        for(j=0; j<3; j++) mid[1][j]=(a[j]+c[j])/2;
-        for(j=0; j<3; j++) mid[2][j]=(a[j]+d[j])/2;
-        for(j=0; j<3; j++) mid[3][j]=(b[j]+c[j])/2;
-        for(j=0; j<3; j++) mid[4][j]=(c[j]+d[j])/2;
-        for(j=0; j<3; j++) mid[5][j]=(b[j]+d[j])/2;
-
-        /* create 4 tetrahedrons by subdivision */
-
-        divide_tetra(a, mid[0], mid[1], mid[2], m-1);
-        divide_tetra(mid[0], b, mid[3], mid[5], m-1);
-        divide_tetra(mid[1], mid[3], c, mid[4], m-1);
-        divide_tetra(mid[2], mid[4], d, mid[5], m-1);
-
+        for(j=0; j<3; j++) v1[j]=(a[j]+b[j])/2;
+        for(j=0; j<3; j++) v2[j]=(a[j]+c[j])/2;
+        for(j=0; j<3; j++) v3[j]=(b[j]+c[j])/2;
+        divide_triangle(a, v1, v2, m-1);
+        divide_triangle(c, v2, v3, m-1);
+        divide_triangle(b, v3, v1, m-1);
     }
-    else(tetra(a,b,c,d)); /* draw tetrahedron at end of recursion */
+    else(triangle(a,b,c)); /* draw triangle at end of recursion */
 }
 
-
-void display()
+void tetrahedron( int m)
 {
+
+/* Apply triangle subdivision to faces of tetrahedron */
+
+	glColor3f(1.0,0.0,0.0);
+    divide_triangle(v[0], v[1], v[2], m);
+	glColor3f(0.0,1.0,0.0);
+    divide_triangle(v[0], v[2], v[3], m);
+	glColor3f(0.0,0.0,1.0);
+    divide_triangle(v[0], v[3], v[1], m);
+	glColor3f(0.0,0.0,0.0);
+    divide_triangle(v[1], v[2], v[3], m);
+}
+
+void display(void)
+{
+
+
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glBegin(GL_TRIANGLES);
-    divide_tetra(v[0], v[1], v[2], v[3], n);
-    glEnd();
+	   glLoadIdentity();
+	   glMatrixMode (GL_PROJECTION);    
+		glLoadIdentity ();    
+		glOrtho(-1.0, 1.0, -1.0, 1.0, -1.0,1.0);  
+    tetrahedron(n);
     glFlush();
 }
 
@@ -100,6 +104,7 @@ void myReshape(int w, int h)
     glMatrixMode(GL_MODELVIEW);
     
 }
+
 
 int WINAPI WinMain (HINSTANCE hInstance,
                     HINSTANCE hPrevInstance,
@@ -129,7 +134,7 @@ int WINAPI WinMain (HINSTANCE hInstance,
 
     /* create main window */
     hWnd = CreateWindow (
-      "3D Gasket", "Simple Triangle", // Simple Triangle為視窗上的名字 
+      "3D Gasket", "Triabgle Change", // Simple Triangle為視窗上的名字 
       WS_CAPTION | WS_POPUPWINDOW | WS_VISIBLE,
       0, 0, 256, 256,// 0,0 表示視窗出現的位置 在微軟工作下會出現在左上角  256,256表示視窗大小 
       NULL, NULL, hInstance, NULL);
@@ -167,9 +172,14 @@ int WINAPI WinMain (HINSTANCE hInstance,
              glColor3f(0.0f,1.0f,0.0f); glVertex2f(-0.5f,0.0f); //定義第2個座標點  顏色為綠色  
              glColor3f(0.0f,0.0f,1.0f); glVertex2f(0.0f,1.0f); //定義第3個座標點  顏色為藍色  
             glEnd();*/
-            n=2; /* or enter number of subdivision steps here */
+            
+
+             /* or enter number of subdivision steps here */
             glEnable(GL_DEPTH_TEST);
-            glClearColor (1.0, 1.0, 1.0, 1.0);
+            glClearColor (0.0, 0.0, 0.0, 0.0);
+           	
+
+            
             display();
             
             SwapBuffers (hDC);
